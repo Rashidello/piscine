52cbf057732fc4f2eb87bd4743a62a8364f6a723
Rashidellø <86264484+Rashidello@users.noreply.github.com>
2024-03-10 02:42:55 +0400

    Add files via upload

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+	char i;
+	i = 'a';
+	while(i <= 'z')
+	{
+		write(1, &i,1);
+		write(1,"\n",1);
+		i++;
+	}
+	write(1,"\n",1);
+}
+
+int main()
+{
+	ft_print_alphabet();
+}
